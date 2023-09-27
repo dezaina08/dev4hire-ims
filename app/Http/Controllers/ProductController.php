918ad3a0caba4609b23810ab26329721cac9c04b
Elvin <elvin08.dez@gmail.com>
2023-09-27 15:14:52 +0800

    Update product index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,6 @@ use Inertia\Inertia;
 use App\Models\Product;
 use App\Models\Category;
 use App\Models\Subcategory;
-use App\Models\PurchaseItem;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\StoreProductRequest;
@@ -23,14 +22,20 @@ class ProductController extends Controller
      */
     public function index(Request $request)
     {
-        // dd($this->getData($request));
+        $categoryFilter = Category::find($request->categoryId);
+        $subcategoryFilter = Subcategory::find($request->subcategoryId);
+
         return Inertia::render('Product/Index', [
             'response' => $this->getData($request),
             'search' => $request->search ?? '',
             'order' => [
                 'orderBy' => $request->orderBy ?? 'id',
                 'orderType' => $request->orderType ?? 'desc'
-            ]
+            ],
+            'dateFrom' => $request->dateFrom ?? '',
+            'dateUntil' => $request->dateUntil ?? '',
+            'category' => $categoryFilter ?? [],
+            'subcategory' => $subcategoryFilter ?? [],
         ]);
     }
 
@@ -45,12 +50,36 @@ class ProductController extends Controller
         ->orderBy($this->tableName . '.' . ($request->orderBy ?? 'id'), $request->orderType ?? 'desc')
         // Search
         ->when($request->search != '', function ($query) use ($request) {
-                return $query->orWhere($this->tableName . '.name', 'like', '%' . $request->search . '%');
+            return $query->where(function ($groupQuery) use ($request) {
+                $groupQuery
+                    ->where($this->tableName . '.id', 'like', '%' . $request->search . '%')
+                    ->orWhere($this->tableName . '.name', 'like', '%' . $request->search . '%')
+                    ->orWhere($this->tableName . '.product_code', 'like', '%' . $request->search . '%')
+                    ->orWhereHas('category', function ($categoryQuery) use ($request) {
+                        $categoryQuery->where('name', 'like', '%' . $request->search . '%');
+                    })
+                    ->orWhereHas('subcategory', function ($subcategoryQuery) use ($request) {
+                        $subcategoryQuery->where('name', 'like', '%' . $request->search . '%');
+                    });
+            });
+        })
+        ->when($request->categoryId != '', function ($query) use ($request) {
+            return $query->where($this->tableName . '.category_id', $request->categoryId);
+        })
+        ->when($request->subcategoryId != '', function ($query) use ($request) {
+            return $query->where($this->tableName . '.subcategory_id', $request->subcategoryId);
+        })
+        ->when($request->dateUntil != '', function ($query) use ($request) {
+            return $query->where($this->tableName . '.created_at', '<=', $request->dateUntil);
+        })
+        ->when($request->dateFrom != '', function ($query) use ($request) {
+            return $query->where($this->tableName . '.created_at', '>=', $request->dateFrom);
         })
         ->paginate($request->perPage ?? 10);
-
+    
         return $query;
     }
+    
 
     /**
      * Show the form for creating a new resource.
@@ -94,7 +123,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        $product->load('category', 'subcategory', 'unit' );
+        $product->load('category', 'subcategory', 'unit');
         return Inertia::render('Product/Show', [
             'model' => $product,
         ]);
--- resources/js/Pages/Product/Index.vue
+++ resources/js/Pages/Product/Index.vue
@@ -26,12 +26,60 @@
                     />
                 </div>
             </div>
+            <div class="flex flex-col lg:flex-row justify-end md:mb-4 gap-y-2">
+                <div
+                    class="flex mb-4 lg:mb-0 flex-col lg:flex-row gap-x-4 gap-y-2"
+                >
+                    <Combobox
+                        id="category_id"
+                        ajaxUrl="/search-category?search="
+                        v-on:update:model-value="updateCategoryId($event)"
+                        inputClass="h-12 m-0"
+                        required
+                        placeholder="Search category"
+                        :defaultSelected="props.category"
+                    />
+                    <Combobox
+                        id="subcategory_id"
+                        :ajaxUrl="'/search-subcategory?category_id=' + props.category?.id + '&search='"
+                        v-on:update:model-value="updateSubcategoryId($event)"
+                        inputClass="h-12"
+                        required
+                        placeholder="Search subcategory"
+                        :defaultSelected="props.subcategory"
+                        :disabled="!props.category?.id"
+                    />
+                    <div class="flex rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-inset focus-within:ring-blue-600 bg-white h-12 w-full md:w-auto">
+                        <span class="flex select-none items-center pl-3 text-gray-500 text-sm">Created From: </span>
+                        <input
+                            type="date"
+                            id="created_from"
+                            autocomplete="off"
+                            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                            placeholder="From"
+                            v-model="dateFrom"
+                        >
+                    </div>
+                    <div class="flex rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-inset focus-within:ring-blue-600 bg-white h-12 w-full md:w-auto">
+                        <span class="flex select-none items-center pl-3 text-gray-500 text-sm">Until: </span>
+                        <input
+                            type="date"
+                            id="created_until"
+                            autocomplete="off"
+                            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                            placeholder="Until"
+                            v-model="dateUntil"
+                        >
+                    </div>
+                </div>
+            </div>
             <Table
                 :url="url"
                 :response="props.response"
                 :table-header="tableHeader"
                 :search="props.search"
                 :order="props.order"
+                :additionalArgumentProp="dateFromArgument + dateUntilArgument + categoryIdArgument + subcategoryIdArgument"
             >
                 <template #tr>
                     <tr
@@ -40,9 +88,6 @@
                         :key="item.id"
                         class="border-b"
                     >
-                        <td class="px-4 py-3 text-gray-700">
-                            {{ item.id }}
-                        </td>
                         <td class="px-4 py-3 font-medium text-gray-700">
                             {{ item.name }}
                         </td>
@@ -50,13 +95,7 @@
                             {{ item.product_code }}
                         </td>
                         <td class="px-4 py-3 font-medium text-gray-700">
-                            {{ item.category.name }}
-                        </td>
-                        <td class="px-4 py-3 font-medium text-gray-700">
-                            {{ item.subcategory.name }}
-                        </td>
-                        <td class="px-4 py-3 font-medium text-gray-700">
-                            {{ item.unit.name }}
+                            {{ item.stock }}
                         </td>
                         <td class="px-4 py-3 font-medium text-gray-700">
                             {{ item.buying_price }}
@@ -65,7 +104,13 @@
                             {{ item.selling_price }}
                         </td>
                         <td class="px-4 py-3 font-medium text-gray-700">
-                            {{ item.stock }}
+                            {{ item.unit.name }}
+                        </td>
+                        <td class="px-4 py-3 font-medium text-gray-700">
+                            {{ item.category.name }}
+                        </td>
+                        <td class="px-4 py-3 font-medium text-gray-700">
+                            {{ item.subcategory.name }}
                         </td>
                         <!-- Actions -->
                         <td class="text-sm text-gray-700 font-light px-4 py-3">
@@ -84,14 +129,14 @@
                                 >
                                     <PencilSquareIcon class="block h-5 w-5" aria-hidden="true" />
                                 </Link>
-                                <a
-                                    href="#"
+                                <button
+                                    type="button"
                                     class="text-red-400 hover:text-white transition duration-200 ease-in-out border border-red-400 hover:border-red-600 rounded-md py-1 px-1 hover:bg-red-500"
                                     title="Delete"
                                     @click="deleteItem.confirmDelete(url, item.id)"
                                 >
                                     <TrashIcon class="block h-5 w-5" aria-hidden="true" />
-                                </a>
+                                </button>
                             </div>
                         </td>
                     </tr>
@@ -121,11 +166,12 @@ import {
     PlusIcon
 } from '@heroicons/vue/24/outline'
 import { ShoppingBagIcon } from '@heroicons/vue/24/solid'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import DynamicLink from '@/Components/DynamicLink.vue'
 import Breadcrumb from '@/Components/Breadcrumb.vue'
 import Table from '@/Components/Table/Table.vue'
 import { useDeleteItemStore } from '@/stores/deleteItem'
+import Combobox from '@/Components/Combobox.vue'
 
 const url = 'products'
 const pageTitle = 'Products'
@@ -144,15 +190,24 @@ const props = defineProps({
             orderType: ''
         }
     },
+    dateFrom: {
+        type: String,
+        default: '',
+    },
+    dateUntil: {
+        type: String,
+        default: '',
+    },
+    category: {
+        type: Object,
+    },
+    subcategory: {
+        type: Object,
+    }
 })
 
 // column = database column name, used for sortBy, null to disable sorting
 const tableHeader = ref([
-    {
-        title: 'ID',
-        class: 'px-4 py-3',
-        column: 'id'
-    },
     {
         title: 'Name',
         class: 'px-4 py-3',
@@ -164,20 +219,11 @@ const tableHeader = ref([
         column: 'product_code'
     },
     {
-        title: 'Category',
+        title: 'Stock',
         class: 'px-4 py-3',
-        column: null
+        column: 'stock'
     },
-    {
-        title: 'Subcategory',
-        class: 'px-4 py-3',
-        column: null
-    },
-    {
-        title: 'Unit',
-        class: 'px-4 py-3',
-        column: 'unit_id'
-    },
+
     {
         title: 'Buying Price',
         class: 'px-4 py-3',
@@ -189,9 +235,19 @@ const tableHeader = ref([
         column: 'selling_price'
     },
     {
-        title: 'Stock',
+        title: 'Unit',
         class: 'px-4 py-3',
-        column: 'stock'
+        column: 'unit_id'
+    },
+    {
+        title: 'Category',
+        class: 'px-4 py-3',
+        column: 'category_id'
+    },
+    {
+        title: 'Subcategory',
+        class: 'px-4 py-3',
+        column: 'subcategory_id'
     },
     {
         title: 'Actions',
@@ -199,6 +255,62 @@ const tableHeader = ref([
         column: null
     },
 ])
+
+// Deep Copy
+const dateFrom = props.dateFrom != ''
+    ? ref(JSON.parse(JSON.stringify(props.dateFrom)))
+    : ref('')
+
+const dateUntil = props.dateUntil != ''
+    ? ref(JSON.parse(JSON.stringify(props.dateUntil)))
+    : ref('')
+
+const categoryId = props.category != ''
+    ? ref(JSON.parse(JSON.stringify(props.category.id)))
+    : ref('')
+
+const subcategoryId = props.subcategory != ''
+    ? ref(JSON.parse(JSON.stringify(props.subcategory.id)))
+    : ref('')
+
+let dateFromArgument = computed(() => {
+    return dateFrom.value != ''
+        ? 'dateFrom=' + dateFrom.value
+        : ''
+})
+
+let dateUntilArgument = computed(() => {
+    return dateUntil.value != ''
+        ? (dateFromArgument.value != ''
+            ? '&'
+            : '') + 'dateUntil=' + dateUntil.value
+        : ''
+})
+
+let categoryIdArgument = computed(() => {
+    return categoryId.value != ''
+        ? (dateFromArgument.value != ''
+            ? '&'
+            : '') + 'categoryId=' + categoryId.value
+        : ''
+})
+
+let subcategoryIdArgument = computed(() => {
+    return subcategoryId.value != ''
+        ? (categoryIdArgument.value != ''
+            ? '&'
+            : '') + 'subcategoryId=' + subcategoryId.value
+        : ''
+})
+
+let updateCategoryId = (event) => {
+    categoryId.value = !!event ? event.id : ''
+    subcategoryId.value = ''
+}
+
+let updateSubcategoryId = (event) => {
+    subcategoryId.value = !!event ? event.id : ''
+}
 </script>
 <style lang="">
 
